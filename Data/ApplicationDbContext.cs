c3882522418ee16ba4e6fe3f7fe41264bd30ca03
Marco Casamento <m.casamento@nettunosolutions.com>
2026-03-11 17:47:00 +0100

    Add missing Data/ApplicationDbContext.cs to repository
    This file was created but never committed, causing Docker build failures in CI/CD

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using GuestAccess.Models;
+
+namespace GuestAccess.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+{
+    public DbSet<AuditLog> AuditLogs { get; set; }
+}
